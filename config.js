3aab6f689387b64ff90d25df39f44d31b086dc3c
Bryan Spearman <bryanspearman@yahoo.com>
2019-01-05 20:09:29 -0500

    update to CLIENT_ORIGIN

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 'use strict';
-exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
-};
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+
 exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL =
   process.env.DATABASE_URL || 'mongodb://localhost:27017/event-listener';
