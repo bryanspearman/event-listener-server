b4fba69accb67407a92ba55d657eded2fba5d7f0
Bryan Spearman <bryanspearman@yahoo.com>
2019-01-06 20:57:29 -0500

    updates to try and fix server errors

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+å;
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
 app.use('/api/items/', itemRouter);
