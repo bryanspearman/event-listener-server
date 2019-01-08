3b2f66d01b39bed56bb30a4e38d237ee9a2932af
Bryan Spearman <bryanspearman@yahoo.com>
2019-01-08 16:45:10 -0500

    updated config for access to localhost

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 'use strict';
-exports.CLIENT_ORIGIN =
-  process.env.CLIENT_ORIGIN || 'https://event-listener-client.herokuapp.com';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.PORT = process.env.PORT || 8080;
 
 exports.DATABASE_URL =
